02f5607a563c6f5b4ae6a60994bfa275d4acb4db
lbda1 <liyangkeda@126.com>
2018-09-21 17:33:06 +0800

    选择排序

---
+++ basic_algorithm/sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+/**
+ * Created by liyang54 on 2018/9/21.
+ * 每次选取最小的，然后在剩余的元素中选择次小的
+ * O(n2)
+ */
+//选择排序
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr={1,3,2,45,65,33,12};
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+        //选择排序的优化
+        for(int i = 0; i < arr.length - 1; i++) {// 做第i趟排序
+            int k = i;
+            for(int j = k + 1; j < arr.length; j++){// 选最小的记录
+                if(arr[j] < arr[k]){
+                    k = j; //记下目前找到的最小值所在的位置
+                }
+            }
+            //在内层循环结束，也就是找到本轮循环的最小的数以后，再进行交换
+            if(i != k){  //交换a[i]和a[k]
+                int temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
+            }
+        }
+        System.out.println();
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+    }
+
+}
